bc81f12246ccaaf0cabbca218be146d80d055f03
Erick <128732040+Erick-Iuri@users.noreply.github.com>
2023-05-09 11:23:55 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,46 @@
+const BotãoStar = document.querySelector(".Botão-go");
+const BotãoStop = document.querySelector(".Stop");
+const BotãoReset = document.querySelector(".Reset");
+
+
+const Timer = document.querySelector(".Display");
+let interval = ""
+let timeleft = 1500;
+
+function update() {
+    let minutes = Math.floor(timeleft / 60);
+    let seconds = timeleft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+        .toString()
+        .padStart(2, "0")}`;
+
+    Timer.innerHTML = formattedTime
+}
+
+function StartTimer() {
+    interval = setInterval(()=>{
+        timeleft--;
+        update();
+        if(timeleft == 0) {
+            clearInterval(interval);
+            alert("Hora do café!");
+            timeleft = 1500;
+            update();
+        }
+    }, 1000)
+}
+
+function StopTimer() {
+    clearInterval(interval);
+}
+
+function ResetB() {
+    clearInterval(interval);
+    timeleft = 1500;
+    update();
+}
+
+BotãoStar.addEventListener("click", StartTimer);
+BotãoStop.addEventListener("click", StopTimer);
+BotãoReset.addEventListener("click", ResetB);
+
